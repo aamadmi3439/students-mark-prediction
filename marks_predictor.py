e48b7aa6ed2821b540a8e0f0f8f6a7d064055c33
CHINMAY PAREEK <132579337+aamadmi3439@users.noreply.github.com>
2023-08-06 18:45:56 +0530

    Create marks_predictor.py

---
+++ marks_predictor.py
@@ -0,0 +1,24 @@
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.utils import shuffle
+
+data = pd.read_csv("downloads\student-mat.csv")
+data.head()
+
+data = data[["G1", "G2", "G3", "studytime", "failures", "absences"]]
+predict = "G3"
+x = np.array(data.drop([predict], 1))
+y = np.array(data[predict])
+
+from sklearn.model_selection import train_test_split
+xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size=0.2)
+linear_regression = LinearRegression()
+linear_regression.fit(xtrain, ytrain)
+accuracy = linear_regression.score(xtest, ytest)
+print(accuracy)
+
+predictions = linear_regression.predict(xtest)
+for i in range(len(predictions)):
+    print(predictions[x], xtest[x], [ytest[x]])
+
